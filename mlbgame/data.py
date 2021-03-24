47a1c60881bbde6809fc7d94521281de3860787f
Nicolai Kozel <nicolaikozel@live.ca>
2021-03-24 14:22:20 -0400

    Don't fail on get_raw_box_score call

--- mlbgame/data.py
+++ mlbgame/data.py
@@ -79,7 +79,8 @@ def get_raw_box_score(game_id):
         return urlopen(GAME_URL.format(year, month, day, game_id,
                                        'rawboxscore.xml'))
     except HTTPError:
-        raise ValueError('Could not find a game with that id.')
+        print(HTTPError)
+        return None
 
 
 def get_game_events(game_id):
